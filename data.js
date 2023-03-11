120d308d84af11b2b39f38df5bcd148412a304ed
Viktor_Bragin <vitya.bragin.1999@mail.ru>
2023-03-11 23:38:50 +0300

    first commit

---
+++ data.js
@@ -0,0 +1,66 @@
+const dataProducts = `[
+    {
+        "img": "img/item1.svg",
+        "nameProduct": "MANGO PEOPLE T-SHIRT",
+        "price": "$300",
+        "color": "Red",
+        "size": "Xl",
+        "quantity": 8
+     },
+     {
+        "img": "img/item2.svg",
+        "nameProduct": "ELLERY X M'O CAPSULE",
+        "price": "$62",
+        "color": "Blue",
+        "size": "X",
+        "quantity": 7
+     },
+     {
+        "img": "img/item2.svg",
+        "nameProduct": "ELLERY X M'O CAPSULE",
+        "price": "$82",
+        "color": "White",
+        "size": "L",
+        "quantity": 6
+     },
+     {
+      "img": "img/item4.svg",
+      "nameProduct": "ELLERY X M'O CAPSULE",
+      "price": "$352",
+      "color": "red",
+      "size": "Xl",
+      "quantity": 5
+   },
+   {
+      "img": "img/item5.svg",
+      "nameProduct": "ELLERY X M'O CAPSULE",
+      "price": "$252",
+      "color": "orange",
+      "size": "XS",
+      "quantity": 4
+   },
+   {
+      "img": "img/item6.svg",
+      "nameProduct": "ELLERY X M'O CAPSULE",
+      "price": "$42",
+      "color": "pink",
+      "size": "S",
+      "quantity": 3
+   },
+   {
+      "img": "img/item7.svg",
+      "nameProduct": "ELLERY X M'O CAPSULE",
+      "price": "$42",
+      "color": "brown",
+      "size": "L",
+      "quantity": 2
+   },
+   {
+      "img": "img/Layer51.svg",
+      "nameProduct": "ELLERY X M'O CAPSULE",
+      "price": "$352",
+      "color": "green",
+      "size": "XXl",
+      "quantity": 1
+   }
+]`
